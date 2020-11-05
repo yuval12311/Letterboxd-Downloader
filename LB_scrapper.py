de23e29aa0c30e169ececc88a90b045e2c41b28c
yuval12311 <yuval.nosov@gmail.com>
2020-11-05 17:17:46 +0200

    Added Letterboxd scrapping for imdb id

---
+++ LB_scrapper.py
@@ -0,0 +1,21 @@
+import string
+
+import requests
+from bs4 import BeautifulSoup
+
+def imdb_id(LB_URL: string):
+    return imdb_id_from_URL(imdb_URL_from_LB(LB_URL))
+
+def imdb_URL_from_LB(LB_URL: string):
+
+    page = requests.get(LB_URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    result = soup.find("a", string="IMDb")
+    return result["href"]
+
+def imdb_id_from_URL(URL: string):
+    return URL.split('/')[4]
+
+
+print(imdb_id_from_URL("http://www.imdb.com/title/tt0181689/maindetails"))
+
